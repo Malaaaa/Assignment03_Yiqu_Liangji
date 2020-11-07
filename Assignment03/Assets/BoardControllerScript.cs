e26ac09ec93e57dccaf7a0bf761aee81630eab38
Elon Zhang <clearlovezyq@gmail.com>
2020-11-07 01:13:41 -0500

    Yiqu Zhang: Add function to calculate the grild number and position.

---
+++ Assignment03/Assets/BoardControllerScript.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardControllerScript : MonoBehaviour
+{
+
+    private RaycastHit raycastHit;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /*
+     *  Calculate the current mouse position in grild
+     *  Rules function could use this, set it to public
+     */
+    public Vector3 CalculateTheGrildPosition() {
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition); 
+        if (Physics.Raycast (ray, out raycastHit, 100)) {
+			Vector3 point = raycastHit.point;
+            int i = (int) point.x;
+            int j = (int) point.z;
+			// Debug.Log(j + (i - 1) * 8);
+            return new Vector3(i, 0, j);
+		}
+        return Vector3.zero;
+    }
+
+    /*
+     *  To make sure the mouse position in the board
+     */
+    public bool IsValidPosition(Vector3 currentPosition) {
+
+        if (currentPosition == Vector3.zero) {
+            return false;
+        }
+        float x = currentPosition.x;
+        float z = currentPosition.z;
+        if (x > 8 || x < 0) {
+            return false;
+        }
+        if (z > 8 || z < 0) {
+            return false;
+        }
+        return true;
+    }
+
+    /*
+     *  Calculate the position in which gril number
+     *  Rules function could use this, set it to public
+     */
+    public int CalculateTheGrildNumber(Vector3 currentPosition) {
+
+        float x = currentPosition.x;
+        float z = currentPosition.z;
+        return (int) (z + (x - 1) * 8);
+    }
+
+    /*
+     *  Check the number whether in the board.
+     */
+    public bool IsValidNumber(int number) {
+        
+        return number >= 1 && number <= 64;
+    }
+
+}
